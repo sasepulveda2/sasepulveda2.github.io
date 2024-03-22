8d37e5525a869682ec1d2b06ea11c875d19468f4
sasepulveda2 <88344205+sasepulveda2@users.noreply.github.com>
2024-03-22 19:55:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 2000
+    });
+});
+
 function updateContent() {
     var userInput = document.getElementById('dynamic-input').value;
     document.getElementById('dynamic-content').textContent = userInput;
